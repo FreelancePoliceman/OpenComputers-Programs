1ee6f89eea24158f8e85fdf549fe2780de378f8e
adam <anoriginale.mailaddress99@gmail.com>
2019-10-01 03:07:54 -0500

    Bug

--- reactor.lua
+++ reactor.lua
@@ -3,11 +3,9 @@ local thread = require ("thread")
 local event = require ("event")
 
 function manage () 
-	while (1) do 
-		local reactor = component.br_reactor
-		local percent_to_fill = math.floor (100 * reactor.getEnergyStored()/reactor.getEnergyCapacity())
-		reactor.setAllControlRodLevels(percent_to_fill)
-	end
+	local reactor = component.br_reactor
+	local percent_to_fill = math.floor (100 * reactor.getEnergyStored()/reactor.getEnergyCapacity())
+	reactor.setAllControlRodLevels(percent_to_fill)
 end
 
 local timer = thread.create ( -- Function for extensibility later, perhaps.
